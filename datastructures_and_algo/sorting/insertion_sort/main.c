645d5a87414839a9befee3ca42c5ce8fce40e1d9
miclemabasie <miclemabasie3@gmail.com>
2023-12-08 23:32:43 +0100

    insertion sort in C

---
+++ datastructures_and_algo/sorting/insertion_sort/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+void generate_array(int arr[]);
+void show_array(int arr[]);
+void insertion_sort(int arr[]);
+
+int size = 50;
+
+int main(void)
+{
+    int list[size];
+    generate_array(list);
+    show_array(list);
+    insertion_sort(list);
+    show_array(list);
+    return 0;
+}
+
+void generate_array(int arr[])
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+void show_array(int arr[])
+{
+    printf("[ ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("] \n");
+}
+
+void insertion_sort(int arr[])
+{
+    for (int i = 1; i < size; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && current > arr[j])
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+}
+
+/*
+
+Algorithm
+The steps to implement the insertion sort algorithm are described below:
+
+1. We assume that the array is empty and on inserting one element into it will not affect the sorting order of the array. Thus, the first element is already sorted.
+
+2. We run a loop from the second element to the last element in the array. In each iteration, the current element is compared with the previous elements, and its correct position is searched.
+
+3. If the current element is greater than or equal to the previous element then we simply move toward the next iteration. If the current element is smaller than previous elements all the elements greater than the current are shifted one position to the right and the current element is placed at its correct position.
+
+4. Step 3 is repeated for each element in the array.
+
+*/

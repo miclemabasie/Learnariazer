fc10d01f491dcc3d6cd442968dc3aacc0a12ec6f
miclemabasie <miclemabasie3@gmail.com>
2023-12-14 19:45:54 +0100

    intro queue

---
+++ datastructures_and_algo/algo_assignment/queue/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+
+    enqueue() -> Add item to the queue
+    dequeue() -> Remove an item from the queue
+
+    peek() -> Gets the element at teh front of the queue
+    isfull() -> check if the queue is full
+    isempty() -> Checks if the queue is empty
+*/
+
+int main()
+{
+
+    return 0;
+}

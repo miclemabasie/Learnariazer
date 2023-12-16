88cf85c0f7919ee48807e112919933b0d546c1cf
miclemabasie <miclemabasie3@gmail.com>
2023-12-16 16:13:56 +0100

    register, login, activate and passwordreset

--- django-real-estate/real_estate/settings/development.py
+++ django-real-estate/real_estate/settings/development.py
@@ -21,3 +21,15 @@ DATABASES = {
         "PORT": env("POSTGRES_PORT"),
     }
 }
+
+
+# Email configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@reale-state.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
